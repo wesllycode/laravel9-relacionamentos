181a7639054d03e1fbd8ad2c82aaa616fcf585da
wesllycode <wesllycode07@gmail.com>
2022-09-13 02:48:57 -0300

    feat: atualizacoes  - Adicionei layout default  - Alterei rota de welcome para home  - Renomei o arquvo welcome para home  - Adcionei o controller CrudOperatorsController

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colocar função do Laravel que pega o Título</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <!-- component -->
+    <!-- Fonte: https://tailwindcomponents.com/component/projects-table -->
+<div class="overflow-x-auto">
+    
+    @yield('content')
+    
+</body>
+</html>
