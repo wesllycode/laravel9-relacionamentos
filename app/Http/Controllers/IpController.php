2e1c20c1cffde4d3c6333d400d0271563be88e64
wesllycode <wesllycode07@gmail.com>
2022-10-23 03:59:20 -0300

    fix: executando aula QueryBuilder

--- app/Http/Controllers/IpController.php
+++ app/Http/Controllers/IpController.php
@@ -8,7 +8,12 @@ use Illuminate\Http\Request;
 class IpController extends Controller
 {
     public function listar(){
-        $ips = DB::select('select * from ips');
-        dd($ips);
+        //  Modo um de usar
+        //  $ips = DB::select('select * from ips WHERE id = ?', [1]);
+        
+        // Outro forma de usar, para puxar os dados e exibir no site
+        //$id = 1;
+        //$ips = DB::table('ips')->where('id',$id)->get();
+        return $ips;
     }
 }
